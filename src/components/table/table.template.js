5280165dd3b0afe3b0d07ec97e2e984c35ee3498
SketchPiece <lubkin.andrey@gmail.com>
2020-06-03 11:38:18 +0300

    Finish base framework functional

---
+++ src/components/table/table.template.js
@@ -0,0 +1,51 @@
+const CODES = {
+  A: 65,
+  Z: 90
+}
+
+function toCell() {
+  return `
+    <div class="cell" contenteditable></div>
+  `
+}
+
+function toColumn(content) {
+  return `
+    <div class="column">${content}</div>
+  `
+}
+
+function createRow(content, info) {
+  return `
+    <div class="row">
+      <div class="row-info">${info || ''}</div>
+      <div class="row-data">${content}</div>
+    </div>
+  `
+}
+
+function toChar(_, i) {
+  return String.fromCharCode(CODES.A + i)
+}
+
+export function createTable(rowsCount = 15) {
+  const colsCount = CODES.Z - CODES.A
+  const rows = []
+
+  const cols = new Array(colsCount)
+      .fill('')
+      .map(toChar)
+      .map(toColumn)
+      .join('')
+
+  rows.push(createRow(cols))
+  for (let i = 0; i < rowsCount; i++) {
+    const cells = new Array(colsCount)
+        .fill('')
+        .map(toCell)
+        .join('')
+    rows.push(createRow(cells, i + 1))
+  }
+
+  return rows.join('')
+}
